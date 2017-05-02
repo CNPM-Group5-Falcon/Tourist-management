943828a33efde0a0d16cdd554e1843f1a7e6443f
14520583 <14520583@gm.uit.edu.vn>
2017-05-02 21:44:59 +0700

    update

---
+++ SourceCode/Nghia/VNTour/PickTour/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace PickTour
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.superGridControl1 = new DevComponents.DotNetBar.SuperGrid.SuperGridControl();
+            this.labelX1 = new DevComponents.DotNetBar.LabelX();
+            this.labelX2 = new DevComponents.DotNetBar.LabelX();
+            this.labelX3 = new DevComponents.DotNetBar.LabelX();
+            this.superGridControl2 = new DevComponents.DotNetBar.SuperGrid.SuperGridControl();
+            this.labelX4 = new DevComponents.DotNetBar.LabelX();
+            this.superGridControl3 = new DevComponents.DotNetBar.SuperGrid.SuperGridControl();
+            this.buttonX1 = new DevComponents.DotNetBar.ButtonX();
+            this.buttonX2 = new DevComponents.DotNetBar.ButtonX();
+            this.SuspendLayout();
+            // 
+            // superGridControl1
+            // 
+            this.superGridControl1.FilterExprColors.SysFunction = System.Drawing.Color.DarkRed;
+            this.superGridControl1.Location = new System.Drawing.Point(12, 63);
+            this.superGridControl1.Name = "superGridControl1";
+            this.superGridControl1.Size = new System.Drawing.Size(273, 64);
+            this.superGridControl1.TabIndex = 0;
+            this.superGridControl1.Text = "superGridControl1";
+            // 
+            // labelX1
+            // 
+            // 
+            // 
+            // 
+            this.labelX1.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.labelX1.Location = new System.Drawing.Point(102, 7);
+            this.labelX1.Name = "labelX1";
+            this.labelX1.Size = new System.Drawing.Size(75, 23);
+            this.labelX1.TabIndex = 1;
+            this.labelX1.Text = "ĐẶT TOUR";
+            // 
+            // labelX2
+            // 
+            // 
+            // 
+            // 
+            this.labelX2.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.labelX2.Location = new System.Drawing.Point(12, 34);
+            this.labelX2.Name = "labelX2";
+            this.labelX2.Size = new System.Drawing.Size(112, 23);
+            this.labelX2.TabIndex = 2;
+            this.labelX2.Text = "CHI TIẾT BẢNG GIÁ";
+            // 
+            // labelX3
+            // 
+            // 
+            // 
+            // 
+            this.labelX3.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.labelX3.Location = new System.Drawing.Point(12, 133);
+            this.labelX3.Name = "labelX3";
+            this.labelX3.Size = new System.Drawing.Size(127, 23);
+            this.labelX3.TabIndex = 4;
+            this.labelX3.Text = "THÔNG TIN LIÊN LẠC";
+            // 
+            // superGridControl2
+            // 
+            this.superGridControl2.FilterExprColors.SysFunction = System.Drawing.Color.DarkRed;
+            this.superGridControl2.Location = new System.Drawing.Point(12, 162);
+            this.superGridControl2.Name = "superGridControl2";
+            this.superGridControl2.Size = new System.Drawing.Size(273, 64);
+            this.superGridControl2.TabIndex = 3;
+            this.superGridControl2.Text = "superGridControl2";
+            // 
+            // labelX4
+            // 
+            // 
+            // 
+            // 
+            this.labelX4.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.labelX4.Location = new System.Drawing.Point(12, 232);
+            this.labelX4.Name = "labelX4";
+            this.labelX4.Size = new System.Drawing.Size(210, 23);
+            this.labelX4.TabIndex = 6;
+            this.labelX4.Text = "DANH SÁCH KHÁCH THAM GIA TOUR";
+            // 
+            // superGridControl3
+            // 
+            this.superGridControl3.FilterExprColors.SysFunction = System.Drawing.Color.DarkRed;
+            this.superGridControl3.Location = new System.Drawing.Point(12, 261);
+            this.superGridControl3.Name = "superGridControl3";
+            this.superGridControl3.Size = new System.Drawing.Size(273, 64);
+            this.superGridControl3.TabIndex = 5;
+            this.superGridControl3.Text = "superGridControl3";
+            // 
+            // buttonX1
+            // 
+            this.buttonX1.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.buttonX1.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.buttonX1.Location = new System.Drawing.Point(192, 341);
+            this.buttonX1.Name = "buttonX1";
+            this.buttonX1.Size = new System.Drawing.Size(75, 23);
+            this.buttonX1.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.buttonX1.TabIndex = 7;
+            this.buttonX1.Text = "XÁC NHẬN";
+            // 
+            // buttonX2
+            // 
+            this.buttonX2.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.buttonX2.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.buttonX2.Location = new System.Drawing.Point(26, 341);
+            this.buttonX2.Name = "buttonX2";
+            this.buttonX2.Size = new System.Drawing.Size(75, 23);
+            this.buttonX2.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.buttonX2.TabIndex = 8;
+            this.buttonX2.Text = "HỦY";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(303, 376);
+            this.Controls.Add(this.buttonX2);
+            this.Controls.Add(this.buttonX1);
+            this.Controls.Add(this.labelX4);
+            this.Controls.Add(this.superGridControl3);
+            this.Controls.Add(this.labelX3);
+            this.Controls.Add(this.superGridControl2);
+            this.Controls.Add(this.labelX2);
+            this.Controls.Add(this.labelX1);
+            this.Controls.Add(this.superGridControl1);
+            this.Name = "Form1";
+            this.Text = "ĐẶT TOUR";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevComponents.DotNetBar.SuperGrid.SuperGridControl superGridControl1;
+        private DevComponents.DotNetBar.LabelX labelX1;
+        private DevComponents.DotNetBar.LabelX labelX2;
+        private DevComponents.DotNetBar.LabelX labelX3;
+        private DevComponents.DotNetBar.SuperGrid.SuperGridControl superGridControl2;
+        private DevComponents.DotNetBar.LabelX labelX4;
+        private DevComponents.DotNetBar.SuperGrid.SuperGridControl superGridControl3;
+        private DevComponents.DotNetBar.ButtonX buttonX1;
+        private DevComponents.DotNetBar.ButtonX buttonX2;
+
+    }
+}
+
