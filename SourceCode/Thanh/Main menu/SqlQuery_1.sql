949c025e8f30e0f245f386127ee70a6ffcebb05c
14520837 <hoangbathanh.it@gmail.com>
2017-05-21 20:56:02 +0700

    main menu + sign_in (rar file)

--- SourceCode/Thanh/Main menu/SqlQuery_1.sql
+++
@@ -1,5 +0,0 @@
-﻿Select info as 'Thông tin', departure as 'Nơi đi', destination as 'Nơi đến', starttime'Khởi hành', duration'Thời gian', fee'Chi phí' From TOUR Where departure = N'Hồ Chí Minh'
---select * from tour where duration = N'3 ngày'
-Select info as 'Thông tin', departure as 'Nơi đi', destination as 'Nơi đến', starttime'Khởi hành', duration'Thời gian', fee'Chi phí' From TOUR Where destination = N'Đà Nẵng'
-select * from TOUR
-select * from tour where destination = N'Nha Trang'
